7f9c03477ad4a1f3a8c439a74abd2d43a0336a5d
dariuszdudek <doodowicz@hotmail.com>
2022-01-29 21:35:23 +0100

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace MemoryGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Memory Game");
+            Console.WriteLine("Input yor choice");
+            Console.WriteLine("1. New game");
+            Console.WriteLine("2. Exit");
+            
+            var notDone = true;
+            while (notDone)
+            {
+                var answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        Console.WriteLine("Game");
+                        notDone = false;
+                        break;
+                    case "2":
+                        Console.WriteLine("Exit");
+                        notDone = false;
+                        break;
+                    default:
+                        Console.WriteLine("I don't understand");
+                        break;
+                }
+            }
         }
     }
 }
